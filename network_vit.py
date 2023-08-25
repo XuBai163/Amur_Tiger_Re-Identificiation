c8f8c6c9f11f9fe9139ef7c6d6db988c0d2e5557
Xufeng Bai <baix0531@gmail.com>
2023-08-25 16:41:01 +0300

    Add ViT network     - Chnage head network to ViT from MGN.

--- network_vit.py
+++ network_vit.py
@@ -1,33 +1,42 @@
 import torch.nn as nn
-from torchvision.models import resnet50
-from transformers import ViTModel
+from transformers import ViTForImageClassification
+from torchvision.models.resnet import resnet50
 
-# $env:PYTHONPATH = "C:/Users/22498/Desktop/feature-aggregation-master/feature aggregation/maskrcnn_benchmark"
+num_classes = 107
 
-class ResNet50ViT(nn.Module):
-    def __init__(self, config, num_classes=1000):
-        super(ResNet50ViT, self).__init__()
-        
+class ViTNetwork(nn.Module):
+    def __init__(self):
+        super(ViTNetwork, self).__init__()
+
+        # Backbone network: ResNet50
         resnet = resnet50(pretrained=True)
+        self.backbone = nn.Sequential(
+            resnet.conv1,
+            resnet.bn1,
+            resnet.relu,
+            resnet.maxpool,
+            resnet.layer1,
+            resnet.layer2,
+            resnet.layer3,
+            resnet.layer4
+        )
 
-        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
-        
-        # resnet_out_features = 2048
-        
-        self.vit = ViTModel(config)
-    
-        vit_out_features = config.hidden_size
-        self.classifier = nn.Linear(vit_out_features, num_classes)
-        
+        # Head network: ViT
+        self.vit = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224-in21k', add_pooling_layer=True)
+        for param in self.vit.parameters():
+            param.requires_grad = True 
+        self.vit = nn.Sequential(*list(self.vit.children())[:-1]) # Remove the classification head
+        self.fc = nn.Linear(768, num_classes)  # Adjust the size depending on the ViT variant's feature size
+
     def forward(self, x):
+        # Extract features from resnet50
         x = self.backbone(x)
-        
-        x = x.flatten(2).transpose(1, 2)
-        
-        x = self.vit(x)
 
-        x = x['last_hidden_state'][:, 0]
+        # Flatten the features and pass to the ViT
+        B, C, H, W = x.shape
+        x = x.view(B, C, H * W).permute(0, 2, 1)  
+        x = self.vit(x)
         
-        x = self.classifier(x)
-        
+        x = self.fc(x)
         return x
+
